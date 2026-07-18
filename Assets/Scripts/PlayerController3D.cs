0d598d6d3b34594113d06a874726a208778e8029
emjaiyee <mjleguidleguid17@gmail.com>
2026-07-18 22:35:34 +0800

    Update PlayerController3D.cs

--- Assets/Scripts/PlayerController3D.cs
+++ Assets/Scripts/PlayerController3D.cs
@@ -16,29 +16,39 @@ public class PlayerController3D : MonoBehaviour
 
     void FixedUpdate() 
     {
+        // Translates user inputs into digital value apparently.
         Vector2 inputVector = moveAction.ReadValue<Vector2>();
 
+        // Line used to convert input into movement direction.
         Vector3 moveDir = transform.right * inputVector.x + transform.forward * inputVector.y;
 
+        // Velocity is deprecated. Use linearVelocity instead.
+        // Line used to apply actual displacement.
         rb.linearVelocity = new Vector3(moveDir.x * moveSpeed, rb.linearVelocity.y, moveDir.z * moveSpeed);
     }
 
     void Update()
     {
+        // New input system, calls the specific input action. It's the new GetKeyDown.
         if (jumpAction.WasPressedThisFrame())
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 
+    // If not called then pressing the input actions will do absolutely nothing apparently.
     void OnEnable()
     {
+        // Correct way of enabling components
         moveAction.Enable();
         jumpAction.Enable();
     }
 
+     
+    // If not called then it can still linger and possibly cause memory leak or something.
     void OnDisable()
     {
+        // Correct way of enabling components
         moveAction.Disable();
         jumpAction.Disable();
     }
