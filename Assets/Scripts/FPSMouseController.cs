786c7adfb715cd05c04f812ff6cdf4fceaf5adfa
emjaiyee <mjleguidleguid17@gmail.com>
2026-07-20 21:15:09 +0800

    Update FPSMouseController.cs

--- Assets/Scripts/FPSMouseController.cs
+++ Assets/Scripts/FPSMouseController.cs
@@ -3,59 +3,6 @@ using UnityEngine.InputSystem;
 
 public class FPSMouseController : MonoBehaviour
 {
-    [SerializeField] InputAction lookAction;
-    [SerializeField] float mouseSensitivity = 20f;
-    [SerializeField] Transform playerBody;
-    private float xRotation = 0f;
 
-    private Rigidbody bodyRigidbody;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //Cursor locking logic.
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-        if (playerBody != null)
-        {
-            bodyRigidbody = playerBody.GetComponent<Rigidbody>();
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 mouseInput = lookAction.ReadValue<Vector2>();
-
-        // Apply mouse movement to x & y
-        float mouseX = mouseInput.x * mouseSensitivity * Time.deltaTime;
-        float mouseY = mouseInput.y * mouseSensitivity * Time.deltaTime;
-
-        // Prevents mouse from inverting in y 
-        xRotation -= mouseY;
-
-        // Clamp 
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        if (bodyRigidbody != null)
-        {
-            Quaternion deltaRotation = Quaternion.Euler(Vector3.up * mouseX);
-
-            bodyRigidbody.MoveRotation(bodyRigidbody.rotation * deltaRotation);
-        }
-
-    }
-
-    void OnEnable()
-    {
-        lookAction.Enable();
-    }
-
-    void OnDisable()
-    {
-        lookAction.Disable();
-    }
 }
