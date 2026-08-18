fda4d9541e7304ed13d4988b89a221ab6ed017f4
emjaiyee <mjleguidleguid17@gmail.com>
2026-08-18 23:31:49 +0800

    New save poiint created equipment manager to handle equipment data

--- Assets/Scripts/Inventory System/EquipmentSlotUI.cs
+++ Assets/Scripts/Inventory System/EquipmentSlotUI.cs
@@ -9,6 +9,7 @@ public class EquipmentSlotUI : MonoBehaviour, IPointerClickHandler
     [SerializeField] private EquipmentType slotType;
     [SerializeField] private RectTransform slotRectTransform;
     [SerializeField] private Image slotIconImage;
+    [SerializeField] private GameObject itemUIPrefab;
 
     public InventoryItem EquippedItem { get; private set; }
     public EquipmentType SlotType => slotType;
@@ -101,7 +102,7 @@ public class EquipmentSlotUI : MonoBehaviour, IPointerClickHandler
 
         dragManager.PickUpItem(itemToPickup, null, visualToPickup);
     }
-
+    
     public void UpdateSlotVisual(InventoryItem item)
     {
         if (item != null && item.Data != null)
